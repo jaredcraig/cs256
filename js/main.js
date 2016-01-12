9a49bc662ed050be87872230d39b36f91d4b0c57
Jared <jaredmcraig@gmail.com>
2016-01-12 10:45:37 -0700

    js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
 $(document).ready(function() {
-	init();
+	$('.button').mouseenter(function() {
+		$(this).css('box-shadow','5px 5px 0px #aaa');
+	});
+	$('.button').mouseleave(function() {
+		$(this).css('box-shadow','5px 5px 3px #bbb');
+	});
+	$('.button').mousedown(function(){
+		$(this).css({
+			'box-shadow': 'none',
+			'left': '+=3px',
+			'top': '+=3px'
+		});
+	});
+	$('.button').mouseup(function(){
+		$(this).css({
+			'box-shadow': '5px 5px 0px #bbb',
+			'left': '-=3px',
+			'top': '-=3px'
+		});
+	});
+	$('#red').click(function() {
+		$('#text').css({
+			'color': 'red',
+			'font-family': 'Times New Roman'
+		});
+	});
+	$('#green').click(function() {
+		$('#text').css({
+			'color': 'green',
+		});
+	});
+	$('#blue').click(function() {
+		$('#text').css({
+			'color': 'blue',
+			'font-family': 'Arial'
+		});
+	});
+	$('#black').click(function() {
+		$('#text').css({
+			'color': 'black',
+		});
+		$('#text').html('Hello there');
+	});
+
 });
-
-function click() {
-	alert("clicked!");
-}
-
-function updateFontColor() {
-
-}
